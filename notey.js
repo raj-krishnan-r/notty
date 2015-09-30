d150b6bf104f78c4b799f7c54b4ac3e83028de51
Raj Krishnan R <rkrishnan1993@live.com>
2015-09-30 22:28:53 +0530

    post and get method on dev.

--- notey.js
+++ notey.js
@@ -12,7 +12,30 @@ notey =
     },
             post:function(data)
     {
-               console.log(Object.keys(data));
+        var http = new XMLHttpRequest();
+        var i=0;
+        	var fd = new FormData();    
+               var keyArray=Object.keys(data);
+               while(typeof(keyArray[i])!="undefined")
+                   {
+                       fd.append(keyArray[i],data[i]);
+                       i++;
+                   }
+                   
+    },
+            get:function(url)
+    {
+        var http = new XMLHttpRequest();
+http.open("GET", url, true);
+http.onreadystatechange = function() {
+	if(http.readyState === 4 && http.status ===200) {
+		return(http.responseText);
+	}
+        	if(http.readyState === 4 && http.status ===404) {
+		alert('404');
+	}
+}
+http.send(null);
     }
         };
 
