233bc34df390197d6651ee763626fafb59701020
Raj Krishnan R <rkrishnan1993@live.com>
2015-10-01 08:51:31 +0530

    AJAX get and post methods added

--- notey.js
+++ notey.js
@@ -10,34 +10,46 @@ notey =
         return document.getElementById(sel.substring(1,sel.length));               
             }
     },
-            post:function(data)
+            /*
+             *Notes get method returns https response object recieved for the URL para
+             -meter pased, to the callback function defined
+             *in the function call like notey.get('url.html',function(data){alert(data)}); 
+             */
+            post:function(url,data,callback)
     {
         var http = new XMLHttpRequest();
         var i=0;
         	var fd = new FormData();    
                var keyArray=Object.keys(data);
-               while(typeof(keyArray[i])!="undefined")
+               while(typeof(keyArray[i])!=='undefined')
                    {
-                       fd.append(keyArray[i],data[i]);
+                       fd.append(keyArray[i],data[keyArray[i]]);
                        i++;
                    }
-                   
+                   http.open("post",url,true);
+                    http.send(fd);
+                    http.onreadystatechange = function() {//Call a function when the state changes.
+	if(http.readyState == 4 && http.status == 200) {
+		callback(http);
+	}
+}
     },
-            get:function(url)
+            get:function(url,callback)
     {
         var http = new XMLHttpRequest();
 http.open("GET", url, true);
 http.onreadystatechange = function() {
 	if(http.readyState === 4 && http.status ===200) {
-		return(http.responseText);
+                callback(http);
 	}
         	if(http.readyState === 4 && http.status ===404) {
-		alert('404');
+                callback(http);
 	}
 }
 http.send(null);
     }
         };
+
 
 
         
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+echo "contents are :";
+echo $_POST['fruit']."<br>";
+echo $_POST['vehicle']."<br>";
+echo $_POST['person']."<br>";
+?>
